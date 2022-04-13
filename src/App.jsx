857e4840a44267d1c6a69748db7984885dffc7cd
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-13 10:10:49 +0200

    chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="text-indigo-600 text-4xl font-extrabold">
+        Hello world
+      </div>
+    </div>
+  )
+}
+
+export default App
