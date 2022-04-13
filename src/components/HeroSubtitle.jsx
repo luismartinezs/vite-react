48543da39d05692a98b64e391963509a7ff747f9
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-13 16:18:27 +0200

    style: responsive font size

--- src/components/HeroSubtitle.jsx
+++ src/components/HeroSubtitle.jsx
@@ -1,6 +1,6 @@
 export default function HeroSubtitle() {
   return (
-    <p className="text-2xl text-gray-600 font-normal">
+    <p className="text-lg lg:text-2xl text-gray-600 font-normal">
       An approachable, performant and versatile framework for building web user
       interfaces.
     </p>
