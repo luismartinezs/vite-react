da7cb23c97fc9de0868cd809d435a99c309ee027
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-13 20:28:13 +0200

    feat: add note section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import HeroSection from '@/components/HeroSection'
 import HeaderSection from './components/HeaderSection'
 import LineSpacer from './components/LineSpacer'
+import NoteSection from './components/NoteSection'
 
 function App() {
   return (
     <div className="font-base text-gray-700">
       <HeaderSection />
       <HeroSection />
+      <LineSpacer />
+      <NoteSection />
       <LineSpacer />
     </div>
   )
---
+++ src/components/NoteSection.jsx
@@ -0,0 +1,13 @@
+export default function NoteSection() {
+  return (
+    <div className="py-4 flex justify-center items-center text-xs text-gray-700 font-medium">
+      Special sponsor
+      <img
+        className="h-10 inline mx-6 hue-rotate-90"
+        src="https://vuejs.org/images/sponsors/hbuilder.avif"
+        alt="HBuilder"
+      />
+      Advanced IDE for React
+    </div>
+  )
+}
