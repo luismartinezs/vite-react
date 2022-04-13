73b883e29582a4884ee28041353f54dc88abec54
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-13 16:28:05 +0200

    feat: add button styles

--- src/components/BaseButton.jsx
+++ src/components/BaseButton.jsx
@@ -5,10 +5,12 @@ export default function BaseButton({
 }) {
   const getButtonClasses = () => {
     const base =
-      'font-semibold px-4 py-2 rounded-lg flex items-center transition ease-in-out duration-300'
+      'font-semibold px-4 py-2 rounded-lg flex items-center transition ease-in-out duration-300 group whitespace-nowrap'
     switch (btnStyle) {
       case 'primary':
         return `${base} bg-primary-500 hover:bg-primary-600 text-white`
+      case 'secondary':
+        return `${base} bg-gray-100 hover:bg-gray-200 text-gray-500`
       default:
         return ''
     }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ import ButtonList from '@/components/ButtonList'
 
 export default function HeroSection() {
   return (
-    <div className="flex flex-col justify-center items-center py-20 md:max-w-4xl max-w-sm mx-auto text-center space-y-8">
+    <div className="flex flex-col justify-center items-center py-20 md:max-w-5xl max-w-sm mx-auto text-center space-y-8">
       <HeroTitle />
       <HeroSubtitle />
       <ButtonList />
