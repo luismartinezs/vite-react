5cb1f7d0ac207c73effe876c9d898706d171fccf
Luis Martinez <luismartinezwebdev@gmail.com>
2022-04-14 13:50:11 +0200

    chore: add template eslint file

---
+++ .eslintrc-medhat.js
@@ -0,0 +1,15 @@
+// this is for nextjs + typescript projects
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended'],
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
+}
